8d11bcad735043eecf4aaaf2415a0be42c1469b0
Ryan <rrichardson610@gmail.com>
2021-12-07 12:37:31 -0700

    day seven brute force no grace solution

---
+++ 2021/day7/dayseven_1.rb
@@ -0,0 +1,26 @@
+a = File.readlines('dayseven_testinput.txt')
+
+def fuel_cost(positions, max)
+  lowest_cost = 1000000000
+  max.times do |index|
+    r = 0
+    positions.each do |pos|
+      r += (index - pos).abs
+    end
+    if r < lowest_cost
+      lowest_cost = r
+    end
+  end
+  lowest_cost
+end
+
+
+### Takes an embarrassing amount of time, but it works!
+
+starting_p = a[0].split(",").map { |hp| hp.to_i }
+
+max_p = starting_p.max
+
+p fuel_cost(starting_p, max_p)
+
+
